81a1b17b88337f4d4a88b71318ec7f176fc02ce6
francescotroiano98 <francesco-troiano-98@libero.it>
2023-09-20 18:11:47 +0200

    added new layout style to the Search section

--- src/components/pages/Search.vue
+++ src/components/pages/Search.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="container">
-  <div class="search-bar">
-    <input type="text" name="text" id="searchText" class="navbar" v-model="searchText" @keyup.enter="newSearch(searchText)">
-    <button @click="newSearch(searchText)" class="custom-btn btn">search</button>
-  </div>
-  <nav class="pagination">
-          <li class="prev" @click="prevPage" v-if="prevPageUrl">
-              Previous
-          </li>
-          <li class="next" @click="nextPage" v-if="nextPageUrl">
-              Next
-          </li>
+  <section class="background_details">
+    <div class="container p-3">
+      <div class="serch_bar_container">
+        <div class="search-bar">
+            <input type="text" name="text" id="searchText" class="navbar" v-model="searchText" @keyup.enter="newSearch(searchText)">
+            <button class="nav_btn" @click="newSearch(searchText)">
+                <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 516.18 516.57"><title>hunters target</title><path d="M382.68,103.93V62.34h24V152C486.33,162.56,536.89,206.3,559.76,284c-8.78,0-16.46.15-24.12-.19-1,0-2.2-2.58-2.67-4.15a139.77,139.77,0,0,0-30.66-54.87q-46.1-50.47-114.55-48.24c-1.51,0-3,0-5.07,0V127.77c-9.63,1.46-18.77,2.11-27.54,4.28Q236.31,161.36,206.39,280.32c-.65,2.6-1.41,4-4.5,3.88-6.36-.28-12.74-.12-19.11-.17a8.64,8.64,0,0,1-1.75-.51c9.15-49.57,32.06-91.37,69.65-124.9S332.31,107.41,382.68,103.93Z" transform="translate(-136.59 -62.34)"/><path d="M357.9,534.26C269.46,520.71,184.14,442.34,178,332.76H136.59V308.84h90.12c4.1-45.81,22.72-84,56.64-114.38,21.33-19.1,46.17-31.68,74.58-38.1v23.91c-71.84,25.1-107.93,75.25-107.7,152.24H202.06c1.25,9,1.74,17.35,3.63,25.4q28.4,120.7,148.62,151.25c2.92.74,4.16,1.67,4,4.83-.25,6.21-.08,12.44-.1,18.66A8.65,8.65,0,0,1,357.9,534.26Z" transform="translate(-136.59 -62.34)"/><path d="M431.34,485V460.31c33.54-9.3,60.72-27.9,80.91-56.27s28.46-60,26.21-95.17h47.85C586,245.58,536.45,152.58,431.42,131V107.49c76.78,9.37,172.36,81.57,180,201.09h41.4v23.88h-90c-4.11,46.15-23,84.56-57.27,115A163.47,163.47,0,0,1,431.34,485Z" transform="translate(-136.59 -62.34)"/><path d="M229.75,357.43h24.63c25.32,72.09,75.42,108.24,152.34,107.79v48.47c10.51-1.67,20.4-2.55,29.95-4.87,64.8-15.8,110.39-54.61,136.57-115.92,4.3-10.07,6.75-20.95,9.69-31.55.86-3.1,1.92-4.31,5.23-4.16,6.5.28,13,.08,19.56.08-8.78,76.17-82.17,173.6-200.77,180.34V578.9H382.7V489.42C303,478.63,252.41,435,229.75,357.43Z" transform="translate(-136.59 -62.34)"/></svg>
+                </span>
+                <span class="text">Search</span>
+            </button>
+      </div>
+      </div>
+      
+      <div class="my_section_hunters container border-1 rounded-4">
+        
+      <nav class="pagination">
+          
+          <button class="nav_btn prev" @click="prevPage" v-if="prevPageUrl">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 516.18 516.57"><title>hunters target</title><path d="M382.68,103.93V62.34h24V152C486.33,162.56,536.89,206.3,559.76,284c-8.78,0-16.46.15-24.12-.19-1,0-2.2-2.58-2.67-4.15a139.77,139.77,0,0,0-30.66-54.87q-46.1-50.47-114.55-48.24c-1.51,0-3,0-5.07,0V127.77c-9.63,1.46-18.77,2.11-27.54,4.28Q236.31,161.36,206.39,280.32c-.65,2.6-1.41,4-4.5,3.88-6.36-.28-12.74-.12-19.11-.17a8.64,8.64,0,0,1-1.75-.51c9.15-49.57,32.06-91.37,69.65-124.9S332.31,107.41,382.68,103.93Z" transform="translate(-136.59 -62.34)"/><path d="M357.9,534.26C269.46,520.71,184.14,442.34,178,332.76H136.59V308.84h90.12c4.1-45.81,22.72-84,56.64-114.38,21.33-19.1,46.17-31.68,74.58-38.1v23.91c-71.84,25.1-107.93,75.25-107.7,152.24H202.06c1.25,9,1.74,17.35,3.63,25.4q28.4,120.7,148.62,151.25c2.92.74,4.16,1.67,4,4.83-.25,6.21-.08,12.44-.1,18.66A8.65,8.65,0,0,1,357.9,534.26Z" transform="translate(-136.59 -62.34)"/><path d="M431.34,485V460.31c33.54-9.3,60.72-27.9,80.91-56.27s28.46-60,26.21-95.17h47.85C586,245.58,536.45,152.58,431.42,131V107.49c76.78,9.37,172.36,81.57,180,201.09h41.4v23.88h-90c-4.11,46.15-23,84.56-57.27,115A163.47,163.47,0,0,1,431.34,485Z" transform="translate(-136.59 -62.34)"/><path d="M229.75,357.43h24.63c25.32,72.09,75.42,108.24,152.34,107.79v48.47c10.51-1.67,20.4-2.55,29.95-4.87,64.8-15.8,110.39-54.61,136.57-115.92,4.3-10.07,6.75-20.95,9.69-31.55.86-3.1,1.92-4.31,5.23-4.16,6.5.28,13,.08,19.56.08-8.78,76.17-82.17,173.6-200.77,180.34V578.9H382.7V489.42C303,478.63,252.41,435,229.75,357.43Z" transform="translate(-136.59 -62.34)"/></svg>
+            </span>
+            <span class="text">Previous</span>
+        </button>
+          
+            <button class="nav_btn next" @click="nextPage" v-if="nextPageUrl">
+            <span class="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 516.18 516.57"><title>hunters target</title><path d="M382.68,103.93V62.34h24V152C486.33,162.56,536.89,206.3,559.76,284c-8.78,0-16.46.15-24.12-.19-1,0-2.2-2.58-2.67-4.15a139.77,139.77,0,0,0-30.66-54.87q-46.1-50.47-114.55-48.24c-1.51,0-3,0-5.07,0V127.77c-9.63,1.46-18.77,2.11-27.54,4.28Q236.31,161.36,206.39,280.32c-.65,2.6-1.41,4-4.5,3.88-6.36-.28-12.74-.12-19.11-.17a8.64,8.64,0,0,1-1.75-.51c9.15-49.57,32.06-91.37,69.65-124.9S332.31,107.41,382.68,103.93Z" transform="translate(-136.59 -62.34)"/><path d="M357.9,534.26C269.46,520.71,184.14,442.34,178,332.76H136.59V308.84h90.12c4.1-45.81,22.72-84,56.64-114.38,21.33-19.1,46.17-31.68,74.58-38.1v23.91c-71.84,25.1-107.93,75.25-107.7,152.24H202.06c1.25,9,1.74,17.35,3.63,25.4q28.4,120.7,148.62,151.25c2.92.74,4.16,1.67,4,4.83-.25,6.21-.08,12.44-.1,18.66A8.65,8.65,0,0,1,357.9,534.26Z" transform="translate(-136.59 -62.34)"/><path d="M431.34,485V460.31c33.54-9.3,60.72-27.9,80.91-56.27s28.46-60,26.21-95.17h47.85C586,245.58,536.45,152.58,431.42,131V107.49c76.78,9.37,172.36,81.57,180,201.09h41.4v23.88h-90c-4.11,46.15-23,84.56-57.27,115A163.47,163.47,0,0,1,431.34,485Z" transform="translate(-136.59 -62.34)"/><path d="M229.75,357.43h24.63c25.32,72.09,75.42,108.24,152.34,107.79v48.47c10.51-1.67,20.4-2.55,29.95-4.87,64.8-15.8,110.39-54.61,136.57-115.92,4.3-10.07,6.75-20.95,9.69-31.55.86-3.1,1.92-4.31,5.23-4.16,6.5.28,13,.08,19.56.08-8.78,76.17-82.17,173.6-200.77,180.34V578.9H382.7V489.42C303,478.63,252.41,435,229.75,357.43Z" transform="translate(-136.59 -62.34)"/></svg>
+            </span>
+            <span class="text">Next</span>
+        </button>
+         
       </nav>
-  <SingleHunter  v-for="hunter in hunters" :hunter="hunter"/>
-</div>
+        <SingleHunter  v-for="hunter in hunters" :hunter="hunter"/>
+      </div>
+      
+  </div>
+  </section>  
   
 </template>
 <script>
@@ -85,24 +108,113 @@ export default {
 
 @use '../../styles/partials/variables.scss' as *;
 
+.serch_bar_container{
+  background-color: $background;
+  width: 50%;
+  border-radius: 2rem;
+  padding: 0 1rem 0 1rem;
+  margin-bottom: 0.7rem;
+ 
+}
+.my_section_hunters{
+  width: 100%;
+  background-color: $background;
+  padding-top: 0 !important;
+  margin-bottom: 2rem;
+}
+
+.nav_btn {
+    
+  width: 190px;
+  height: 50px;
+  padding: 0 15px;
+  border-radius: 5px;
+  border: none;
+  transition: all 0.5s ease-in-out;
+  font-size: 20px;
+  font-family: 'Cinzel Decorative', cursive;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  background-color: $primary;
+  color: $background;
+
+  &:active{
+    background: $secondary;
+  }
+  &.next{
+              margin-left: auto;
+          }
+  &.prev{
+              margin-right: auto;
+              flex-direction: row-reverse;
+              .text{
+                transform: translateX(-50px);
+              }
+  }        
+ }
+
+
+.nav_btn:hover {
+  box-shadow: 0 0 20px 0px $secondary;
+}
+
+.nav_btn .icon {
+  position: absolute;
+  width: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s;
+
+  svg{
+    height: 30px;
+  }
+}
+
+.nav_btn .text {
+  transform: translateX(55px);
+
+  a{
+    text-decoration: none;
+    color: $background;
+  }
+
+}
+
+.nav_btn:hover .icon {
+  width: 160px;
+}
+
+.nav_btn:hover .text {
+  transition: all 0.5s;
+  opacity: 0;
+}
+
+.nav_btn:focus {
+  outline: none;
+}
+
+.nav_btn:active .icon {
+  transform: scale(0.85);
+}
+
+
+.background_details{
+  background-color: $secondary;
+  padding-top: 6rem;
+}
+
 .pagination{
       width: 100%;
       padding: 1rem;
       display: flex;
       // justify-content: space-between;
       align-items: center;
-      background-color: black;
+      
       list-style-type: none;
+      
 
-      li{
-          padding: 1rem;
-          background-color: blue;
-          color: white;
-
-          &.next{
-              margin-left: auto;
-          }
-      }
   }
 
 #header{
@@ -118,117 +230,21 @@ font-size:1.5rem;
 font-style:italic;
 }
 .navbar{
-width: 30%;
 border-radius: 10px;
-margin-right: 3rem;
-height: 30px;
-padding-left: 10px;
+margin-right: 1rem;
+width: 24rem;
+
 }
 .search-bar{
 display: flex;
+justify-content: space-between;
 align-items: center;
 width: 100%;
-margin-bottom: 3rem;
 margin-left: 3rem;
-}
-.custom-btn {
-width: 130px;
-height: 40px;
-color: white;
-border-radius: 5px;
-padding: 10px 25px;
-font-family: 'Lato', sans-serif;
-font-weight: 500;
-background: transparent;
-cursor: pointer;
-transition: all 0.3s ease;
-position: relative;
-display: inline-block;
-box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, .5),
-      7px 7px 20px 0px rgba(0, 0, 0, .1),
-      4px 4px 5px 0px rgba(0, 0, 0, .1);
-outline: none;
-}
-
-.btn {
-background: linear-gradient(0deg, rgb(58, 56, 53) 0%, rgb(0, 0, 0) 100%);
-line-height: 42px;
-padding: 0;
-border: none;
-}
-
-.btn span {
-position: relative;
-display: block;
-width: 100%;
-height: 100%;
-}
-
-.btn:before,
-.btn:after {
-position: absolute;
-content: "";
-right: 0;
-bottom: 0;
-background: #2b2a33;
-box-shadow: -7px -7px 20px 0px rgba(255, 255, 255, .9),
-      -4px -4px 5px 0px rgba(255, 255, 255, .9),
-      7px 7px 20px 0px rgba(0, 0, 0, .2),
-      4px 4px 5px 0px rgba(0, 0, 0, .3);
-transition: all 0.3s ease;
-}
-
-.btn:before {
-height: 0%;
-width: 2px;
-}
-
-.btn:after {
-width: 0%;
-height: 2px;
-}
-
-.btn:hover:before {
-height: 100%;
-}
-
-.btn:hover:after {
-width: 100%;
-}
-
-.btn span:before,
-.btn span:after {
-position: absolute;
-content: "";
-left: 0;
-top: 0;
-box-shadow: -7px -7px 20px 0px rgba(255, 255, 255, .9),
-      -4px -4px 5px 0px rgba(255, 255, 255, .9),
-      7px 7px 20px 0px rgba(0, 0, 0, .2),
-      4px 4px 5px 0px rgba(0, 0, 0, .3);
-transition: all 0.3s ease;
-}
-
-.btn span:before {
-width: 2px;
-height: 0%;
-}
-
-.btn span:after {
-height: 2px;
-width: 0%;
-}
-
-.btn span:hover:before {
-height: 100%;
-}
-
-.btn span:hover:after {
-width: 100%;
+margin: 1rem 0 1rem 0;
 }
 .container{
-padding-top: 15rem;
-  background-color: $background;
+padding-top: 2rem;
 width:100%;
 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 height: 100%;
@@ -288,6 +304,7 @@ transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 
 .card:hover .card-text {
 transform: rotateX(0deg);
+
 }
 .card-meal-type{
 font-style:italic;
