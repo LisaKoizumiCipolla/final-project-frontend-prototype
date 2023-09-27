3f222785d4c3d787716caaacef97f0a5dcaf7e2a
francescotroiano98 <francesco-troiano-98@libero.it>
2023-09-27 18:49:13 +0200

    div

--- src/components/pages/SingleHunter.vue
+++ src/components/pages/SingleHunter.vue
@@ -7,7 +7,9 @@
       </h1>
       <div class="card-style col-12 col-md-3 me-3" v-for="singlehunter in store.huntersList">
           <div class="card-img">
-            <div class="image-overlay"></div>
+            <div class="image-overlay">
+
+            </div>
             <img v-if="singlehunter.image.startsWith('http')" :src="singlehunter.image" alt="">
             <img v-else :src="'http://127.0.0.1:8000/storage/' + singlehunter.image" alt="">
           </div>
@@ -16,10 +18,11 @@
                     <p class="title">{{ singlehunter.surname }}</p>
                     <p class="subtitle">{{ singlehunter.name }}</p>
                   </router-link>
-          </div>
+                </div>
+            </div>
+       </div>
+       
       </div>
-    </div>
-  </div>
     
     
       
